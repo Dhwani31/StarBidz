6e7a64d390328b8b8cffba3a5f71202a242d3d31
Apurv Patel <69913001+Apurv103@users.noreply.github.com>
2023-03-23 22:30:32 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/starbidz/About.java
@@ -0,0 +1,21 @@
+package com.iss.starbidz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+
+    @Override
+    public void onBackPressed()
+    { }
+
+
+}
